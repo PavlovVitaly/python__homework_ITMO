79698457812197c8a2f9968754f5ecd416014d55
Pavlov Vitaly <pvs.mazilla@gmail.com>
2017-10-01 16:20:11 +0300

    Lesson 03. Second task is done.

---
+++ lesson03_hw/task_01_02.py
@@ -0,0 +1,18 @@
+print('Введите количество тарелок: ')
+number_of_palates = int(input())
+print('Введите количество моющего средства (в ед.): ')
+number_of_detergent = float(input())
+
+current_number_of_palates = number_of_palates
+current_number_of_detergent = number_of_detergent
+
+while(current_number_of_palates > 0) and (current_number_of_detergent >= 0.5):
+    current_number_of_palates = current_number_of_palates - 1
+    current_number_of_detergent = current_number_of_detergent - 0.5
+
+if (current_number_of_palates > 0) and (current_number_of_detergent < 0.5):
+    print('Моющее средство закончилось. Осталось', current_number_of_palates, 'тарелок')
+elif (current_number_of_palates == 0) and (current_number_of_detergent >= 0.5):
+    print('Все тарелки вымыты. Осталось', current_number_of_detergent, 'ед. моющего средства')
+else:
+    print('Все тарелки вымыты, моющее средство закончилось')
