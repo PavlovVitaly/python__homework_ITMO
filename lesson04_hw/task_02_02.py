bafccb4e5e22051db45dff9085b077f05c95faf0
Pavlov Vitaly <pvs.mazilla@gmail.com>
2017-10-03 18:48:31 +0300

    Fourth homework. Wrote some comment in Task 2.

--- lesson04_hw/task_02_02.py
+++ lesson04_hw/task_02_02.py
@@ -1,9 +1,10 @@
 def bubble_sort(lst):
     output_list = list(lst)
     n = 1
-    while n < len(output_list):
-        for i in range(len(output_list) - n):
-            if output_list[i] > output_list[i + 1]:
+    while n < len(output_list):  # Сужаем неотсортированныю часть массива.
+        for i in range(len(
+                output_list) - n):  # Сравниваем соседние элементы в этом цикле. Самые большие значения будут "оседать" в конце массива.
+            if output_list[i] > output_list[i + 1]:  # Если следующий элемент меньше текущего, то меняем их местами.
                 output_list[i], output_list[i + 1] = output_list[i + 1], output_list[i]
         n += 1
     return output_list
