004ce6dcec2b9b283be579039689bc148dde47fc
Pavlov Vitaly <pvs.mazilla@gmail.com>
2017-10-11 18:38:11 +0300

    Without trigger.

--- Notebook/notebook/schema.sql
+++ Notebook/notebook/schema.sql
@@ -1,16 +1,8 @@
 CREATE TABLE IF NOT EXISTS notebook (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name_of_note TEXT NOT NULL,
-  text_of_note TEXT NOT NULL DEFAULT '',
-  status TEXT NOT NULL DEFAULT '',
-  start_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  end_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-};
-
-
-CREATE TRIGGER trigger_update_status
-    ON notebook AFTER INSERT, UPDATE
-    AS IF UPDATE(start_date, end_date)
-    BEGIN
-
-    END
+  id           INTEGER PRIMARY KEY                                                                                                                                                                         AUTOINCREMENT,
+  name_of_note TEXT     NOT NULL,
+  text_of_note TEXT NOT NULL                                                                                                                                                                               DEFAULT '',
+  status       TEXT NOT NULL                                                                                                                                                                               DEFAULT 'Выполняется',
+  start_date   DATETIME NOT NULL                                                                                                                                                                           DEFAULT CURRENT_TIMESTAMP,
+  end_date     DATETIME NOT NULL                                                                                                                                                                           DEFAULT CURRENT_TIMESTAMP
+)
