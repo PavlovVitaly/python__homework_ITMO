6810d1edac2d92f6b2e33a17af1d728fd4f19630
Pavlov Vitaly <pvs.mazilla@gmail.com>
2017-10-11 18:52:51 +0300

    Storage is added.

--- Notebook/notebook/schema.sql
+++ Notebook/notebook/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS notebook (
-  id           INTEGER PRIMARY KEY                                                                                                                                                                         AUTOINCREMENT,
+  id           INTEGER PRIMARY KEY                                                                                     AUTOINCREMENT,
   name_of_note TEXT     NOT NULL,
-  text_of_note TEXT NOT NULL                                                                                                                                                                               DEFAULT '',
-  status       TEXT NOT NULL                                                                                                                                                                               DEFAULT 'Выполняется',
-  start_date   DATETIME NOT NULL                                                                                                                                                                           DEFAULT CURRENT_TIMESTAMP,
-  end_date     DATETIME NOT NULL                                                                                                                                                                           DEFAULT CURRENT_TIMESTAMP
+  text_of_note TEXT NOT NULL                                                                                           DEFAULT "",
+  status       INT  NOT NULL                                                                                           DEFAULT 1,
+  start_date   DATETIME NOT NULL                                                                                       DEFAULT CURRENT_TIMESTAMP,
+  end_date     DATETIME NOT NULL                                                                                       DEFAULT CURRENT_TIMESTAMP
 )
